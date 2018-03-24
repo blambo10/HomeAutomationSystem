b7460f365f116aa02a56a6bbacc635c6403e596e
blambo10 <37164299+blambo10@users.noreply.github.com>
2018-03-25 09:20:08 +1100

    Classes for HASagent

---
+++ Classes/AutomateMe.py
@@ -0,0 +1,62 @@
+import configparser
+import platform
+import os
+from Classes.Network import Network
+
+class AutomateMe:
+
+    def __init__(self):
+        #Defining "Constants" if you can call a mutable variable that ... sigh.
+        CONST_CONFIG = "hasagent.config"
+        CONST_OS = platform.system()
+        print(CONST_OS)
+
+        #Defining control commands
+        if CONST_OS == "Windows":
+            self.CONST_REBOOTCOMMAND = "shutdown -r -f"
+            self.CONST_SHUTDOWNCOMMAND = "shutdown -s -f"
+        elif CONST_OS == "Linux":
+            self.CONST_REBOOTCOMMAND = "sync;shutdown -r now"
+            self.CONST_SHUTDOWNCOMMAND = "sync;shutdown -h now"
+
+        #else:
+        #    print("Operating Not Supported!")
+
+        #Loading Config Parameters from external config file.
+        self.config = configparser.ConfigParser()
+        networkparameters = self.loadconfig(CONST_CONFIG)
+
+        #Create the network object with appropriate network
+        #Data in the construct
+        self.Listener = Network(*networkparameters)
+
+    def loadconfig(self, configfile):
+        try:
+            #Parsing in config file
+            self.config.read(configfile)
+
+            for key in self.config["NETWORK"]:
+                print(key + " : " + self.config["NETWORK"][key])
+
+            if self.config["NETWORK"]["SocketIP"]:
+                ip = self.config["NETWORK"]["SocketIP"]
+
+            if self.config["NETWORK"]["SocketPort"]:
+                port = self.config["NETWORK"]["SocketPort"]
+
+            #Returning IP and Port extracted from config file
+            #in tuple
+            return ip, port
+        except:
+            return "",""
+
+    def startagent(self):
+        print(self.Listener.listeningip + " : " + self.Listener.listeningport)
+        self.Listener.startsocketlistener(self)
+
+    def powercontroler(self, command):
+        if str(command) == "reboot":
+            os.system(self.CONST_REBOOTCOMMAND)
+        elif str(command) == "shutdown:":
+            os.system(self.CONST_SHUTDOWNCOMMAND)
+
---
+++ Classes/Network.py
@@ -0,0 +1,77 @@
+import platform
+import socket
+import ipaddress
+from socket import error as socket_error
+
+# Network Class
+#
+# Author: Bryce Lamborne
+# Purpose is to provide an Network Object that
+# Contains all appropriate Network Functions
+# As Methods.
+#
+# Including but not limited to a listener.
+class Network:
+
+    def __init__(self, IP, Port):
+        self.__listeningIP = None
+        self.__listeningPort = None
+        self.listeningip = IP
+        self.listeningport = Port
+
+    # Validation Methods
+    def validateIPv4(self, IPAddress):
+        try:
+            socket.inet_aton(IPAddress)
+            return True
+        except:
+            return False
+
+    #Defining Properties
+    @property
+    def listeningip(self):
+        return self.__listeningIP
+
+    @listeningip.setter
+    def listeningip(self, value):
+        if self.validateIPv4(value):
+            self.__listeningIP = value
+            return True
+        else:
+            return False
+
+    @property
+    def listeningport(self):
+        return self.__listeningPort
+
+    @listeningport.setter
+    def listeningport(self, value):
+        self.__listeningPort = value
+
+    #Lets Get this party started
+    def startsocketlistener(self, feedhandler):
+        try:
+            print("Listening on :" + self.listeningip + ":" + self.listeningport)
+            print("OS :" + platform.system())
+            #Creating Socket for Windows
+            #binding to socket
+            #listening on socket
+            HomeAutomationSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            HomeAutomationSocket.bind((self.listeningip, int(self.listeningport)))
+            HomeAutomationSocket.listen(5)
+
+            #Recieving Data Stream From Socket
+            while 1:
+                c, address = HomeAutomationSocket.accept()
+                data = c.recv(2048)
+
+                #send data back to feed handler
+                #to operate appropriate logic dependant pon payload
+                feedhandler.powercontroler(data.decode('utf-8'))
+                #print(data)
+
+            return True
+        except:
+            raise socket_error
+            print("Error with socket")
+            return False
